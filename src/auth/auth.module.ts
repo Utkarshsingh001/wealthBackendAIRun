26e65f004152aa495444ebb0ad5418cda36fb7ea
Rajat Goswami <rajat@wormsconnect.com>
2025-05-07 01:20:13 +0530

    improvment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtAuthService } from './jwt.service';
 
 @Module({
-    providers : [JwtAuthService]
+    // providers : [JwtAuthService]
 })
 export class AuthModule {}
